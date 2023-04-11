e11ea277ec0d08ce60a49241be3990bc02aa4387
Nic Jackson <jackson.nic@gmail.com>
2023-04-11 12:32:07 +0100

    Update vscode to run on port 80

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,35 +27,15 @@ resource "google_compute_firewall" "rules" {
 
   allow {
     protocol  = "tcp"
-    ports     = ["8000", "3000", "22"]
+    ports     = ["80", "3000", "22"]
   }
 
   source_ranges = ["0.0.0.0/0"]
   target_tags = ["shipyard"]
 }
 
-resource "google_dns_managed_zone" "example-zone" {
-  name        = "workshop"
-  dns_name    = "workshop-${random_id.rnd.hex}.com."
-  description = "Example DNS zone"
-}
-
 resource "random_id" "rnd" {
   byte_length = 4
-}
-
-resource "google_compute_firewall" "rules_ipv6" {
-  name        = "shipyard-ingress-ipv6"
-  network     = "default"
-  description = "Creates firewall rule targeting tagged instances"
-
-  allow {
-    protocol  = "tcp"
-    ports     = ["8000", "3000", "22"]
-  }
-
-  source_ranges = ["::/0"]
-  target_tags = ["shipyard"]
 }
 
 resource "google_compute_instance" "default" {
@@ -80,10 +60,6 @@ resource "google_compute_instance" "default" {
       // Ephemeral public IP
     }
     
-    ipv6_access_config {
-      // Ephemeral public IP
-      network_tier = "STANDARD"
-    }
   }
 
   metadata_startup_script = templatefile("${path.module}/cloud-init.sh",{})
@@ -93,14 +69,12 @@ resource "google_compute_instance" "default" {
   }
 }
 
-resource "cloudflare_record" "default" {
-  count = 1
-
+resource "cloudflare_record" "worker0" {
   zone_id = "8542f7e55a8c0cd9c215478cf157e613"
-  name    = "${count.index}-vault-workshop"
-  value   = google_compute_instance.default[count.index].network_interface.0.access_config.0.nat_ip
+  name    = "0-workshop"
+  value   = google_compute_instance.default.0.network_interface.0.access_config.0.nat_ip
   type    = "A"
-  proxied = true
+  proxied = false
 }
 
 output "public_ips" {
